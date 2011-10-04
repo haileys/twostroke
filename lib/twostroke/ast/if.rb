884432ef7a99414b0109479bbc6d74488fd8fb4a
charliesome <charlie@charliesomerville.com>
2011-10-04 21:24:59 +1100

    removed pry from the legit code

--- lib/twostroke/ast/if.rb
+++ lib/twostroke/ast/if.rb
@@ -1,10 +1,8 @@
-require 'pry'
 module Twostroke::AST
   class If < Base
     attr_accessor :condition, :then, :else
     
     def collapse
-      pry binding if condition.nil? || @then.nil?
       self.class.new condition: condition.collapse, then: @then.collapse, else: @else && @else.collapse
     end
   end
