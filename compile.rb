31f781689759ccd16a0b3ee5d1407a3d1527b815
charliesome <charlie@charliesomerville.com>
2011-10-21 19:52:59 +1100

    fix compile.rb

--- compile.rb
+++ compile.rb
@@ -4,7 +4,19 @@ require "twostroke"
 parser = Twostroke::Parser.new(Twostroke::Lexer.new(File.read ARGV.first))
 parser.parse
 
-compiler = Twostroke::Compiler.new parser.statements
+compiler = Twostroke::Compiler::TSASM.new parser.statements
 compiler.compile
 
-puts compiler.src
+require "pry"
+pry binding
+
+#=begin
+compiler.bytecode.each do |section,instructions|
+  puts "#{section}:"
+  instructions.each do |ins|
+    puts "    #{ins[0]}#{" " * (12 - ins[0].size)}#{ins.drop(1).map { |x| x.is_a?(String) ? x.inspect : x }.join ", "}"
+  end
+end
+#=end
+
+#print compiler.tscode
