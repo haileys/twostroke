93d9b463949d3b31a6a4ebc5c234e60c8133a37e
charliesome <charlie@charliesomerville.com>
2012-01-11 12:17:01 +1100

    shorthand lambda syntax: a => a * a; (a,b) => a + b;

--- lib/twostroke/tokens.rb
+++ lib/twostroke/tokens.rb
@@ -85,6 +85,7 @@ module Twostroke
       [ :AMPERSAND, /&/ ],
       [ :OR, /\|\|/ ],
       [ :PIPE, /\|/ ],
+      [ :LAMBDA, /=>/ ],
       [ :TRIPLE_EQUALS, /===/ ],
       [ :DOUBLE_EQUALS, /==/ ],
       [ :EQUALS, /=/ ],
