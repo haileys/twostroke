7ecb3d029bf1619016a0a56a8df38c90ea9e4ea9
charliesome <charlie@charliesomerville.com>
2012-01-27 12:56:53 +1100

    it turns out 0 and -0 are equal

--- lib/twostroke/runtime/types/number.rb
+++ lib/twostroke/runtime/types/number.rb
@@ -10,14 +10,7 @@ module Twostroke::Runtime::Types
     end
     
     def ===(other)
-      if number.zero? && other.is_a?(Number) && other.number.zero?
-        # in javascript, -0 and 0 are not equal
-        # in ruby they are, and the only way to check if a number is -0 is with #to_s
-        # please correct me if there's a better way
-        number.to_s[0] == other.number.to_s[0]
-      else
-        other.is_a?(Number) && number == other.number
-      end
+      other.is_a?(Number) && number == other.number
     end
     
     def typeof
